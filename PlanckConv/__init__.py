54d7be89cc78788f9a2adf231bd059be859f2045
Camille <camille.demay@proton.me>
2026-09-24 17:46:42 +0200

    Update __init__.py

--- PlanckConv/__init__.py
+++ PlanckConv/__init__.py
@@ -1,16 +1,17 @@
+import logging
+import os
+
 from PlanckConv.classes import *
 from PlanckConv.core_functions import *
 from PlanckConv.external_qp_planck import *
-
-import logging, os
 
 _pkg = logging.getLogger("PlanckConv")
 
 if not logging.getLogger().handlers:
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(
-        "%(asctime)s %(levelname)s %(name)s: %(message)s"
-    ))
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s %(levelname)s %(name)s: %(message)s")
+    )
     root = logging.getLogger()
     root.addHandler(handler)
 
